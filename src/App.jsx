013aa7d4f946a876720d78f79b66144be1b6e306
ayubmichaelangelo <111202113797@mhs.dinus.ac.id>
2024-04-23 15:35:27 +0700

    final terakhir

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Home from "./pages/home/Home";
+import Home from "./pages/home/home";
 import Login from "./pages/login/Login";
-import List from "./pages/list/List";
+import List from "./pages/list/list";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
