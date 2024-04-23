e85a6a9c9beff5c4c2ecb637c193df1383793130
ayubmichaelangelo <111202113797@mhs.dinus.ac.id>
2024-04-23 14:38:12 +0700

    ubah satu

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,12 @@
-import './home.scss'
+import "./home.scss";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <Sidebar />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
